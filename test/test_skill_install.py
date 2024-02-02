10252ebfc003326f96f2bfc9f0249d4f99569548
emphasize <swen_g@t-online.de>
2024-02-02 16:02:30 +0100

    toss commented osm test

--- test/test_skill_install.py
+++ test/test_skill_install.py
@@ -44,27 +44,6 @@ def get_skill_object(bus: FakeBus, path: str = "",
     return skill
 
 
-# @unittest.skipIf(environ.get("TEST_OSM", "false") == "false", "TEST_OSM is false")
-# class TestOSM(unittest.TestCase):
-#     def test_osm_install(self):
-#         branch = environ.get("TEST_BRANCH")
-#         install_url = f"https://github.com/{environ.get('TEST_REPO')}@{branch}"
-#         skill = SkillEntry.from_github_url(install_url)
-#         tmp_skills = "/tmp/osm_installed_skills"
-#         skill_folder = f"{tmp_skills}/{skill.uuid}"
-
-#         if exists(skill_folder):
-#             rmtree(skill_folder)
-
-#         updated = skill.install(folder=tmp_skills, default_branch=branch)
-#         self.assertEqual(updated, True) 
-#         self.assertTrue(isdir(skill_folder))
-#         self.assertTrue(isfile(join(skill_folder, "__init__.py")))
-
-#         updated = skill.install(folder=tmp_skills, default_branch=branch)
-#         self.assertEqual(updated, False)
-
-
 class TestSkillLoading(unittest.TestCase):
     @classmethod
     def setUpClass(self):
